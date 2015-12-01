d89c4953fc793cab3bd1e5e8a30a05458b934358
Spikerman <spikechenhao@163.com>
2015-12-01 16:24:02 +0800

    MainUI 初始化线程移入 EDT

--- src/LoginControl.java
+++ src/LoginControl.java
@@ -1,4 +1,7 @@
+import sun.awt.resources.awt;
+
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by chenhao on 12/1/15.
@@ -22,11 +25,16 @@ public class LoginControl {
             if (account.getUserName().equals(loginScreen.accountField.getText()) && account.getPassword().equals(loginScreen.passwordField.getText())) {
                 SaleSystem saleSystem = new SaleSystem(account);
 
-                JOptionPane.showMessageDialog(null, "Welcome: " + account.getClerkName()+" !", "", JOptionPane.PLAIN_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Welcome: " + account.getClerkName() + " !", "", JOptionPane.PLAIN_MESSAGE);
                 loginScreen.setVisible(false);
                 MainUI dialog = new MainUI(saleSystem);
-                dialog.pack();
-                dialog.setVisible(true);
+
+                EventQueue.invokeLater(() -> {
+                    dialog.pack();
+                    dialog.setVisible(true);
+                });
+
+
                 return;
 
             }
